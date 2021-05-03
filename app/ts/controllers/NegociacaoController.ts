85a1746f4cc51b5ec00729bae26062c30f14b49c
Thiago Lanza <lanzathm@gmail.com>
2021-05-03 20:34:20 -0300

    Add negociacoes feature created

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,12 +3,16 @@ class NegociacaoController {
   private _inputData: HTMLInputElement;
   private _inputQuantidade: HTMLInputElement;
   private _inputValor: HTMLInputElement;
+  private _negociacoes = new Negociacoes();
+  private _negociacoesView = new NegociacoesView('#negociacoesView');
 
   constructor() {
     // <HTMLInputElement> é o casting explícito para converter em uma tipagem mais específica
     this._inputData = <HTMLInputElement>document.querySelector('#data');
     this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
     this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+    // Ao instanciar o controller renderiza os dados baseado nas negociações.
+    this._negociacoesView.update(this._negociacoes);
   }
 
   adiciona(event: Event): void {
@@ -23,6 +27,8 @@ class NegociacaoController {
       +this._inputValor.value
     );
 
-    console.log(negociacao);
+    this._negociacoes.adiciona(negociacao);
+
+    this._negociacoesView.update(this._negociacoes);
   }
 }
---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,15 @@
+class Negociacoes {
+
+    // _negociacoes é um array que contém instâncias da Classe Negociacao.
+    private _negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao): void {
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray(): Negociacao[] {
+
+        // Devolve um novo array que é cópia do original.
+        return [].concat(this._negociacoes);
+    }
+}
