8ab05dcde964e0200edd399deb0164b2af64102a
Thiago Lanza <lanzathm@gmail.com>
2021-05-10 17:18:45 -0300

    Compiled files

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,8 +1,19 @@
-class View {
-    constructor(seletor) {
-        this._elemento = $(seletor);
-    }
-    update(modelo) {
-        this._elemento.html(this.template(modelo));
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var View;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            View = class View {
+                constructor(seletor) {
+                    this._elemento = $(seletor);
+                }
+                update(modelo) {
+                    this._elemento.html(this.template(modelo));
+                }
+            };
+            exports_1("View", View);
+        }
+    };
+});
