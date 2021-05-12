8a489c8d85899839e1a1b20a53535169c44eba9b
Thiago Lanza <lanzathm@gmail.com>
2021-05-12 11:18:47 -0300

    Import data button created

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -3,3 +3,4 @@ import { NegociacaoController } from './controllers/NegociacaoController';
 const controller = new NegociacaoController();
 
 $('.form').submit(controller.adiciona.bind(controller)); // Bind permite o uso do this do controller
+$('#botao-importa').click(controller.importaDados.bind(controller));
